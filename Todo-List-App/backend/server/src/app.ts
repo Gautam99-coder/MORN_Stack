8e2e06b286e108b83a59713ef9e6355a15869d55
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2025-04-12 23:21:34 +0530

    Create app.ts

---
+++ Todo-List-App/backend/server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import todosRouter from './routes/todos';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/todos', todosRouter);
+
+export default app;
