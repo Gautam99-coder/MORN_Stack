ae108002af596952424ad041fc1fb33286896429
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-08 23:06:30 +0530

    1

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const _ =  require('lodash');
-let array =[1,2,2,3,4,4,5];
-console.log(_.uniq(array));
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    if (req.url === "/") {
+        res.end("Welcome to PizzaPalooza! 🍕 Try /pepperoni or /cheese!");
+    } else if (req.url === "/pepperoni") {
+        res.end("Here’s your spicy pepperoni pizza! 🌶️🍕");
+    } else if (req.url === "/cheese") {
+        res.end("Cheesy goodness coming right up! 🧀🍕");
+    } else {
+        res.writeHead(404);
+        res.end("Oops! No such pizza on the menu! 😅");
+    }
+});
+
+server.listen(3000, () => {
+    console.log("Pizza shop is open at http://localhost:3000!");
+});
