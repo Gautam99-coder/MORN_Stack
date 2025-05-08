29f5bd594a7478d31d19ce26214c0af113cc129f
Gautam99-coder <gautamtharu1000@gmail.com>
2025-05-08 21:18:13 +0530

    this is simple node app

---
+++ app.js
@@ -0,0 +1,3 @@
+const _ =  require('lodash');
+let array =[1,2,2,3,4,4,5];
+console.log(_.uniq(array));
---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello from Node.js Server!\n');
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
